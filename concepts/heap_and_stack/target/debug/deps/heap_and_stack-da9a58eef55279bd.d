3c40ece2bf863eadb25eff2ce891e0cc1fe7e60d
ibrahim-ibrahim <folahanmi0001@gmail.com>
2025-10-15 14:15:29 +0100

    concept: heap and stack

--- concepts/heap_and_stack/target/debug/deps/heap_and_stack-da9a58eef55279bd.d
+++
@@ -1,5 +0,0 @@
-/Users/admin/Documents/Development/Rust/rust-knowedge/concepts/heap_and_stack/target/debug/deps/heap_and_stack-da9a58eef55279bd.d: src/main.rs
-
-/Users/admin/Documents/Development/Rust/rust-knowedge/concepts/heap_and_stack/target/debug/deps/heap_and_stack-da9a58eef55279bd: src/main.rs
-
-src/main.rs:
--- concepts/heap_and_stack/target/debug/heap_and_stack.d
+++
@@ -1,1 +0,0 @@
-/Users/admin/Documents/Development/Rust/rust-knowedge/concepts/heap_and_stack/target/debug/heap_and_stack: /Users/admin/Documents/Development/Rust/rust-knowedge/concepts/heap_and_stack/src/main.rs
